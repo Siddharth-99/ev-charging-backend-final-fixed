467f0c70252dcfe59bd5aa5aece5854691a3aac6
Siddharth-99 <143240585+Siddharth-99@users.noreply.github.com>
2025-06-04 02:00:23 +0530

    Your commit message here

--- backend/server.js
+++ backend/server.js
@@ -29,11 +29,9 @@ app.use('/api/feedback', feedbackRoutes); // ✅ lowercase
 // ✅ MongoDB
 // mongoose.connect('mongodb://localhost:27017/ev-charging')
 console.log('MONGO_URI:', process.env.MONGO_URI);
+console.log('Loaded MONGO_URI:', JSON.stringify(process.env.MONGO_URI));
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('MongoDB connected');
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
